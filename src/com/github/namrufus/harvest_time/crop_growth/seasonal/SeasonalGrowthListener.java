a5a9998668b828d1d064dc3148358ba0986942c1
nrfs <namrufus@gmail.com>
2013-11-01 18:40:49 -0500

    Minor cleanup for seasonal growth interactions.

--- src/com/github/namrufus/harvest_time/crop_growth/seasonal/SeasonalGrowthListener.java
+++ src/com/github/namrufus/harvest_time/crop_growth/seasonal/SeasonalGrowthListener.java
@@ -64,105 +64,118 @@ public class SeasonalGrowthListener implements Listener {
 		if (toolMaterial == null/*nothing in hand*/)
 			return;
 		
-		// .. growth check interaction ................................................................................
+		// react differently depending on the item in the player's hand
+		Player player = event.getPlayer();
 		if (toolMaterial == interactionConfiguration.getCropGrowthCheckMaterial()) {
-			Player player = event.getPlayer();
-			CropSeasonalGrowthConfiguration cropGrowthConfiguration = seasonalCropList.getBlockCrop(blockMaterial);
-					
-			int growthStage = GrowthUtil.getStage(block);
-					
-			int seasonalDay = (int)calendar.getSeasonalDay();
-			int stageCount = GrowthUtil.getStageCount(blockMaterial);
-			int targetStage = cropGrowthConfiguration.getCappedTargetStage(seasonalDay, stageCount);
-			boolean allowedToGrow = cropGrowthConfiguration.isAllowedToGrow(growthStage, seasonalDay);
-					
-			player.sendMessage("§7[Harvest Time] Crop: §8"+blockMaterial.toString());
-			player.sendMessage("§7[Harvest Time]   Year: §8" + calendar.getSeasonalYear() + "§7, Day: " + "§8" + seasonalDay);
-			player.sendMessage("§7[Harvest Time]   Current growth stage: §8"+ growthStage + "§7 / §8" + (stageCount-1));
-			player.sendMessage("§7[Harvest Time]   Target growth stage: §8" + targetStage + "§7 / §8" + (stageCount-1));
-			if (allowedToGrow) {
-				int finalViableSeasonalDay = cropGrowthConfiguration.getFinalViableSeasonalDay(growthStage);
-				
-				System.out.println("final seasonal day = " + finalViableSeasonalDay);
-				
-				Date date = calendar.getSeasonalDayStart(calendar.getSeasonalYear(), finalViableSeasonalDay + 1);
-				
-				player.sendMessage("§7[Harvest Time]   §3Crop is viable!§7 Tend within: " + "§8" + formatTimeDifference(date) + " (" + date + ")");
-			} else if (cropGrowthConfiguration.isAtTargetStage(growthStage, seasonalDay, stageCount)) {
-				String message = "§7[Harvest Time]   §3Crop at target growth.";
-				if (!(growthStage == stageCount - 1)) {
-					Date date;
-						if (growthStage == 0)
-							date = calendar.getSeasonalDayStart(calendar.getSeasonalYear(), cropGrowthConfiguration.getStartDay() + 1);
-						else
-							date = calendar.nextSeasonalDayIncrement();
-					message += "§7 Next target growth update: " + "§8" + formatTimeDifference(date) + "(" + date + ")";
-				}
-				player.sendMessage(message);
-			} else {
-				player.sendMessage("§7[Harvest Time]   §3Crop is not viable.");
-			}
-			
-		// .. yield check interaction .................................................................................
+			growthCheckInteraction(player, block);
 		} else if (toolMaterial == interactionConfiguration.getCropYieldCheckMaterial()) {
-			Player player = event.getPlayer();
-			CropSeasonalGrowthConfiguration cropGrowthConfiguration = seasonalCropList.getBlockCrop(blockMaterial);
-			
-			if (!cropGrowthConfiguration.hasCustomYield())
-				return;
-			
-			double targetYield = cropGrowthConfiguration.getCustomYieldConfiguration().getYieldCount(block, regionalGenerator);
-			
-			player.sendMessage("§7" + "[Harvest Time] Crop: " + "§8"/*dark grey*/ + blockMaterial.toString());
-			cropGrowthConfiguration.getCustomYieldConfiguration().displayState(player, block, regionalGenerator);
-			player.sendMessage("§7" + "[Harvest Time]   §3Total Yield: " + "§b" + String.format("%.2f", targetYield));
-			
-		// .. seasonal crop tending and custom yield ..................................................................
+			yieldCheckInteraction(player, block);
 		} else if (tendingConfiguration.isTendingTool(toolMaterial)) {
-			CropSeasonalGrowthConfiguration cropGrowthConfiguration = seasonalCropList.getBlockCrop(blockMaterial);
-			
-			// delay event until the time determined by the type of tool
-			double tendingTime = tendingConfiguration.getToolTendingTime(toolMaterial);
-			if (!playerInteractionDelayer.canBreak(event.getPlayer(), block, tendingTime, Math.min(tendingTime, 1.0)))
-				return;
-			
-			int seasonalDay = (int)calendar.getSeasonalDay();
-			int previousGrowth = GrowthUtil.getStage(block);
-			
-			// update the crop clicked
-			updateGrowth(block, seasonalDay, false);
-			
-			int currentGrowth =  GrowthUtil.getStage(block);
-			
-			// handle custom yields on the transition to the final stage
-			if (previousGrowth != currentGrowth && currentGrowth == GrowthUtil.getStageCount(blockMaterial) - 1) {
+			cropTendInteraction(player, toolMaterial, block);
+		}
+	}
+	
+	// display information relating to the growth status of the block to the player.
+	private void growthCheckInteraction(Player player, Block block) {
+		Material blockMaterial = block.getType();
+		CropSeasonalGrowthConfiguration cropGrowthConfiguration = seasonalCropList.getBlockCrop(blockMaterial);
 				
+		int growthStage = GrowthUtil.getStage(block);
 				
-				if (cropGrowthConfiguration.hasCustomYield()) {
-					double targetYield = cropGrowthConfiguration.getCustomYieldConfiguration().getYieldCount(block, regionalGenerator);
-					
-					if (CropYieldUtil.cropFailureSample(blockMaterial, targetYield)) {
-						// the crop has failed, destroy the crop block
-						// reduce the growth stage first, so that the yield is zero
-						GrowthUtil.setStage(block, 0);
-						block.breakNaturally();
-					} else {				
-						// if the crop did not fail, then there may be additional bonus yields
-						List<ItemStack> bonusItemDrops = CropYieldUtil.sampleBonusYield(blockMaterial, targetYield);
-						for (ItemStack item : bonusItemDrops) {
-							block.getWorld().dropItemNaturally(block.getLocation(), item);
-						}
+		int seasonalDay = (int)calendar.getSeasonalDay();
+		int stageCount = GrowthUtil.getStageCount(blockMaterial);
+		int targetStage = cropGrowthConfiguration.getCappedTargetStage(seasonalDay, stageCount);
+		boolean allowedToGrow = cropGrowthConfiguration.isAllowedToGrow(growthStage, seasonalDay);
+				
+		player.sendMessage("§7[Harvest Time] Crop: §8"+blockMaterial.toString());
+		player.sendMessage("§7[Harvest Time]   Year: §8" + calendar.getSeasonalYear() + "§7, Day: " + "§8" + seasonalDay);
+		player.sendMessage("§7[Harvest Time]   Current growth stage: §8"+ growthStage + "§7 / §8" + (stageCount-1));
+		player.sendMessage("§7[Harvest Time]   Target growth stage: §8" + targetStage + "§7 / §8" + (stageCount-1));
+		if (allowedToGrow) {
+			int finalViableSeasonalDay = cropGrowthConfiguration.getFinalViableSeasonalDay(growthStage);
+			
+			System.out.println("final seasonal day = " + finalViableSeasonalDay);
+			
+			Date date = calendar.getSeasonalDayStart(calendar.getSeasonalYear(), finalViableSeasonalDay + 1);
+			
+			player.sendMessage("§7[Harvest Time]   §3Crop is viable!§7 Tend within: " + "§8" + formatTimeDifference(date) + " (" + date + ")");
+		} else if (cropGrowthConfiguration.isAtTargetStage(growthStage, seasonalDay, stageCount)) {
+			String message = "§7[Harvest Time]   §3Crop at target growth.";
+			if (!(growthStage == stageCount - 1)) {
+				Date date;
+					if (growthStage == 0)
+						date = calendar.getSeasonalDayStart(calendar.getSeasonalYear(), cropGrowthConfiguration.getStartDay() + 1);
+					else
+						date = calendar.nextSeasonalDayIncrement();
+				message += "§7 Next target growth update: " + "§8" + formatTimeDifference(date) + "(" + date + ")";
+			}
+			player.sendMessage(message);
+		} else {
+			player.sendMessage("§7[Harvest Time]   §3Crop is not viable.");
+		}		
+	}
+	
+	// display information relating to the yield of the crop to the given player
+	private void yieldCheckInteraction(Player player, Block block) {
+		Material blockMaterial = block.getType();
+		CropSeasonalGrowthConfiguration cropGrowthConfiguration = seasonalCropList.getBlockCrop(blockMaterial);
+		
+		if (!cropGrowthConfiguration.hasCustomYield())
+			return;
+		
+		double targetYield = cropGrowthConfiguration.getCustomYieldConfiguration().getYieldCount(block, regionalGenerator);
+		
+		player.sendMessage("§7" + "[Harvest Time] Crop: " + "§8"/*dark grey*/ + blockMaterial.toString());
+		cropGrowthConfiguration.getCustomYieldConfiguration().displayState(player, block, regionalGenerator);
+		player.sendMessage("§7" + "[Harvest Time]   §3Total Yield: " + "§b" + String.format("%.2f", targetYield));
+	}
+	
+	// update plant growth by "tending" the crop
+	private void cropTendInteraction(Player player, Material toolMaterial, Block block) {
+		Material blockMaterial = block.getType();
+		CropSeasonalGrowthConfiguration cropGrowthConfiguration = seasonalCropList.getBlockCrop(blockMaterial);
+		
+		// delay event until the time determined by the type of tool
+		double tendingTime = tendingConfiguration.getToolTendingTime(toolMaterial);
+		if (!playerInteractionDelayer.canBreak(player, block, tendingTime, Math.min(tendingTime, 1.0)))
+			return;
+		
+		int seasonalDay = (int)calendar.getSeasonalDay();
+		int previousGrowth = GrowthUtil.getStage(block);
+		
+		// update the crop clicked
+		updateGrowth(block, seasonalDay, false);
+		
+		int currentGrowth =  GrowthUtil.getStage(block);
+		
+		// handle custom yields on the transition to the final stage
+		if (previousGrowth != currentGrowth && currentGrowth == GrowthUtil.getStageCount(blockMaterial) - 1) {
+			
+			
+			if (cropGrowthConfiguration.hasCustomYield()) {
+				double targetYield = cropGrowthConfiguration.getCustomYieldConfiguration().getYieldCount(block, regionalGenerator);
+				
+				if (CropYieldUtil.cropFailureSample(blockMaterial, targetYield)) {
+					// the crop has failed, destroy the crop block
+					// reduce the growth stage first, so that the yield is zero
+					GrowthUtil.setStage(block, 0);
+					block.breakNaturally();
+				} else {				
+					// if the crop did not fail, then there may be additional bonus yields
+					List<ItemStack> bonusItemDrops = CropYieldUtil.sampleBonusYield(blockMaterial, targetYield);
+					for (ItemStack item : bonusItemDrops) {
+						block.getWorld().dropItemNaturally(block.getLocation(), item);
 					}
 				}
 			}
-			
-			// update crops in a radius around the updated block, excluding crops that are transitioning to the final stage
-			int y = block.getY();
-			int radius = tendingConfiguration.getRadius();
-			for (int x = block.getX() - radius; x <= block.getX() + radius; x++) {
-				for (int z = block.getZ() - radius; z <= block.getZ() + radius; z++) {
-					updateGrowth(block.getWorld(), x, y, z, seasonalDay, true);
-				}
+		}
+		
+		// update crops in a radius around the updated block, excluding crops that are transitioning to the final stage
+		int y = block.getY();
+		int radius = tendingConfiguration.getRadius();
+		for (int x = block.getX() - radius; x <= block.getX() + radius; x++) {
+			for (int z = block.getZ() - radius; z <= block.getZ() + radius; z++) {
+				updateGrowth(block.getWorld(), x, y, z, seasonalDay, true);
 			}
 		}
 	}
